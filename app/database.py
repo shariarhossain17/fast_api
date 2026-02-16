8c3af2cdf805a867433a13212f3ee576119e073f
shariar17 <shariarhossain23@gmail.com>
2026-02-16 16:18:52 +0600

    create a engine and session

--- app/database.py
+++ app/database.py
@@ -1,5 +1,37 @@
 import os
 
 from dotenv import load_dotenv
-from sqlachemy.orm import DeclarativeBase, sessionmaker
 from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+
+load_dotenv()
+DATABASE_URL=os.getenv('DATABASE_URL')
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+#create database engine
+
+engine=create_engine(
+    DATABASE_URL,
+    echo=False,
+    future=True
+)
+
+
+#create a session
+
+SessionLocal=sessionmaker(
+    bind=engine,
+    autoflush=False
+    autocommit=False
+    future=True
+
+)
+
+
+
+
+
